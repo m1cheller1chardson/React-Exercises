7c389773434554d3687a995ef2b348b12bdef5ab
Michelle <michellerichardson@gmail.com>
2023-07-26 17:58:22 -0400

    javascript if/else, ternary, switch operators

--- day01.js
+++ day01.js
@@ -85,7 +85,6 @@ itCompanies.pop()
 
 // Remove all IT companies
 itCompanies.slice(0, itCompanies.length())
-<<<<<<< HEAD
 
 // First remove all the punctuations and change the string to array and count the number of words in the array
 // ["I", "love", "teaching", "and", "empowering", "people", "I", "teach", "HTML", "CSS", "JS", "React", "Python"] - 13
@@ -145,5 +144,62 @@ const backEnd = ['Node', 'Express', 'MongoDB']
 
 console.log(fullStack)
 ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
-=======
->>>>>>> a175050e9de3f2b9c8fd80f39786115e9fb02892
+
+// Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+    // Enter your age: 30
+    // You are old enough to drive.
+
+    // Enter your age:15
+    // You are left with 3 years to drive.
+
+    let userAgeInput = prompt("Enter your age: ")
+    if (userAgeInput >= 18) {
+        console.log('You are old enough to drive')
+    } else {
+        let yearsToWait = 18 - userAgeInput
+        console.log('You are left with ' + yearsToWait + 'years to drive.')
+    }
+
+// Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+    // Enter your age: 30
+    // You are 5 years older than me.
+
+    let userAgeInputt = prompt("Enter your age: ")
+    let myAge = 26
+    if(userAgeInput > myAge) {
+        let ageDifference = userAgeInput - myAge
+        console.log('You are ' + ageDifference + ' years older than me')
+    } else {
+        let yearsYounger = myAge - userAgeInput
+        console.log('You are ' + yearsYounger + ' years younger than me')
+    }
+
+// If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in two ways: using if else and ternary operator.
+    // let a = 4
+    // let b = 3
+
+    if (a > b) {
+        console.log('a is greater than b')
+    } else {
+        console.log('a is less than b')
+    }
+
+    let isGreater = true
+    isGreater
+        ? console.log('a is greater than b')
+        : console.log('a is less than b')
+
+// Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
+
+    // Enter a number: 2
+    // 2 is an even number
+
+    // Enter a number: 9
+    // 9 is is an odd number.
+
+    let numberInput = prompt('Enter a number: ')
+    if ((numberInput / 2) != 0){
+        console.log(numberInput + ' is an odd number.')
+    } else {
+        console.log(numberInput + ' is an even number.')
+    }
